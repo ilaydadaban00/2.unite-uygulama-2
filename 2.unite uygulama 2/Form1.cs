571bd3a01c65267360caf0fbedef942fe00a3dac
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2024-02-23 02:19:55 +0300

    Add files via upload

---
+++ 2.unite uygulama 2/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2.unite_uygulama_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int yas;
+            yas = Convert.ToInt32(textBox1.Text);
+            if (yas > 17)
+            {
+                MessageBox.Show("ehliyet alabilirsin");
+            }
+        }
+    }
+}
